a62efc8677927bad71925b1a9553f96c6920add4
tony.stukota <tony.stukota@softermii.com>
2018-11-12 17:57:36 +0200

    changes

--- src/components/TransactionItem.js
+++ src/components/TransactionItem.js
@@ -1,15 +1,12 @@
 import React from "react";
-import moment from "moment";
+// import moment from "moment";
 
 import { TransactionItemWrapper } from "./styled";
 
 const TransactionItem = ({ data }) => {
-  const date = moment(data.dateTime).fromNow();
-  console.log(data.dateTime);
-  console.log(new Date());
   return (
     <TransactionItemWrapper>
-      <span>{date}</span>
+      {/* <span>{date}</span> */}
       <span>{data.description}</span>
       <span>$ {data.amount}></span>
     </TransactionItemWrapper>
@@ -17,3 +14,31 @@ const TransactionItem = ({ data }) => {
 };
 
 export default TransactionItem;
+// .toISOString()
+
+// const transDate = new Date(data.dateTime);
+// const todayStart = new Date().setHours(0, 0, 0, 0);
+// const yesterday = new Date().setDate(new Date().getDate() - 1);
+// const yesterdayStart = new Date(yesterday).setHours(0, 0, 0, 0);
+// console.log(transDate < yesterdayStart);
+// const date =
+//   transDate < yesterdayStart
+//     ? transDate.getDate() + " " + months[transDate.getMonth()]
+//     : transDate > todayStart
+//       ? "Today"
+//       : "Yesterday";
+
+// var months = [
+//   "January",
+//   "February",
+//   "March",
+//   "April",
+//   "May",
+//   "June",
+//   "July",
+//   "August",
+//   "September",
+//   "October",
+//   "November",
+//   "December"
+// ];
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -6,13 +6,36 @@ import { TransactionsWrapper } from "./styled";
 
 const tabs = ["last", "details", "transfer"];
 
-const Transactions = ({ data }) => (
-  <TransactionsWrapper>
-    <Tabs tabs={tabs} />
-    {data.map(i => (
-      <TransactionItem data={i} key={i.id} />
-    ))}
-  </TransactionsWrapper>
-);
+const Transactions = ({ data }) => {
+  const todaysTrans = [],
+    yesterdaysTrans = [],
+    oldTrans = [];
+
+  const todayStart = new Date().setHours(0, 0, 0, 0);
+  var yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const yesterdayStart = new Date(yesterday).setHours(0, 0, 0, 0);
+
+  for (var i = 0, len = data.length, stop = false; i < len; i++) {
+    if (stop) oldTrans.push(data[i]);
+    else {
+      const transDate = new Date(data.dateTime);
+      if (transDate < yesterdayStart) {
+        oldTrans.push(data[i]);
+        stop = true;
+      } else {
+      }
+    }
+  }
+
+  return (
+    <TransactionsWrapper>
+      <Tabs tabs={tabs} />
+      {data.map(i => (
+        <TransactionItem data={i} key={i.id} />
+      ))}
+    </TransactionsWrapper>
+  );
+};
 
 export default Transactions;
